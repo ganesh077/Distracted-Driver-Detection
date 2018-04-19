355ae314edfde2c93a8f0e533c98ba3643c3640b
Ganesh Thampi <ganeshthampi077@gmail.com>
2018-04-19 08:30:02 +0530

    Update ClosedEye.py

--- ClosedEye.py
+++ ClosedEye.py
@@ -1,11 +1,7 @@
 import numpy as np
 import cv2
 import pyglet
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('/home/ganesh/opencv-3.1.0/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
 
 cap = cv2.VideoCapture(0)
